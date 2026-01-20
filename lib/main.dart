9964e91e9c7592714f146e02620a3a96fe89c935
PriyankaSinghWishGeeks <priyanka@wishgeekstechserve.com>
2026-01-20 21:35:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'splash_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Flutter Web Working ✅',
-            style: TextStyle(fontSize: 28),
-          ),
-        ),
+      
+      title: 'Zomato Clone',
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(1, 223, 7, 7),
+        scaffoldBackgroundColor: Color.fromARGB(1, 243, 242, 242),
       ),
+
+      home: const SplashScreen(),
+     
     );
   }
 }
+
